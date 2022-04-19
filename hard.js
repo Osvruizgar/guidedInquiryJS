eeff43537f15b01627a83183ab402204b04c212b
Osvaldo <osvruizgar@gmail.com>
2022-04-18 23:05:38 -0400

    added some comments and made sure I understood how it works

--- hard.js
+++ hard.js
@@ -3,18 +3,24 @@ function add(a, b) {
     console.log(a + b);
 };
 
-function subtract() {
-    console.log(10 - 5);
+function subtract(bobby, dan) {
+    console.log(bobby - dan);
 };
 
-function multiply() {
-    console.log(10 * 5);
+function multiply(water, bottle) {
+    console.log(water * bottle);
 };
 
-function divide() {
-    console.log(10 / 5);
+function divide(java, script) {
+    console.log(java / script);
 };
+//a = 10 and b = 5. Function is made to add the two.
 add(10, 5);
-subtract(10, 5);
-multiply(10, 5);
-divide(10, 5);
+//bobby = 10 and dan = 3. Function subtracts the two.
+subtract(10, 3);
+// water = 10 and bottle = 4. function subtracts the two.
+multiply(10, 4);
+// java = 10 and script = 2. 
+divide(10, 2);
+// functions should have names that represent what the function is or does but this is for a reminder.
+// I have these meaningless names to help me out so they do have a meaning. it reminds me that the names in a function are empty variables.
