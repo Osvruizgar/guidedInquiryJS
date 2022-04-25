c36eee354c7e77f1ef565935e4ab253a7710dc79
Osvaldo <osvruizgar@gmail.com>
2022-04-24 20:56:20 -0400

    went back and worked on the code to understand how functions work.

--- veryh.js
+++ veryh.js
@@ -6,31 +6,68 @@ let secondNum = prompt("select your second Number 0-9");
 let total;
 
 if (selectedOperator == "+") {
-    function add(firstNum, secondNum) {
-        console.log(firstNum + secondNum);
+    function add(a, b) {
+        return (a + b);
     };
-    add(Number(firstNum), Number(secondNum));
-    alert(`${firstNum} + ${secondNum} = ${Number(firstNum) + Number(secondNum)}`);
-}
-else if (selectedOperator == "-") {
-     function subtract(firstNum, secondNum) {
-         console.log(firstNum - secondNum);
-        };
-        subtract(Number(firstNum), Number(secondNum));
-        alert(`${firstNum} - ${secondNum} = ${Number(firstNum) - Number(secondNum)}`);
-}
+    total = add(Number(firstNum), Number(secondNum));
+    alert(`${firstNum} + ${secondNum} = ${total}`);
+} 
+else if (selectedOperator == "-"){
+    function subtract(c, d) {
+        return (c - d);
+    };
+    total = subtract(Number(firstNum), Number(secondNum));
+    alert (`${firstNum} - ${secondNum} = ${total}`);
+} 
 else if (selectedOperator == "*") {
-    function multiple(firstNum, secondNum) {
-        console.log(firstNum * secondNum);
-    };
-    multiple(Number(firstNum), Number(secondNum));
-    alert(`${firstNum} * ${secondNum} = ${Number(firstNum) * Number(secondNum)}`);
-} else if (selectedOperator == "/") {
-    function divide(firstNum, secondNum) {
-        console.log(firstNum / secondNum);
-    };
-    divide(Number(firstNum), Number(secondNum));
-    alert(`${firstNum} / ${secondNum} = ${Number(firstNum) / Number(secondNum)}`);
-} else{
-    alert("please make sure you are selecting the correct numbers and operations");
-};
+    function multiple (e, f) {
+        return (e * f);
+    }
+    total = multiple(Number(firstNum), Number(secondNum));
+    alert (`${firstNum} * ${secondNum} = ${total}`);
+} 
+else if (selectedOperator == "/") {
+    function divide (g, h) {
+        return (g / h);
+    }
+    total = divide(Number(firstNum), Number(secondNum));
+    alert (`${firstNum} / ${secondNum} = ${total}`);
+} else {
+    alert("you did something wrong..")
+}
+
+// code mentor taught me below
+//let total;
+// 
+// function add(first, second) {
+//     return first + second;
+// };
+// if (selectedOperator == "+") {  
+//   total = add(Number(firstNum), Number(secondNum));
+//   alert(`${firstNum} + ${secondNum} = ${total}`);
+// }
+
+//old code below
+
+// else if (selectedOperator == "-") {
+//      function subtract(firstNum, secondNum) {
+//          console.log(firstNum - secondNum);
+//         };
+//         subtract(Number(firstNum), Number(secondNum));
+//         alert(`${firstNum} - ${secondNum} = ${Number(firstNum) - Number(secondNum)}`);
+// }
+// else if (selectedOperator == "*") {
+//     function multiple(firstNum, secondNum) {
+//         console.log(firstNum * secondNum);
+//     };
+//     multiple(Number(firstNum), Number(secondNum));
+//     alert(`${firstNum} * ${secondNum} = ${Number(firstNum) * Number(secondNum)}`);
+// } else if (selectedOperator == "/") {
+//     function divide(firstNum, secondNum) {
+//         console.log(firstNum / secondNum);
+//     };
+//     divide(Number(firstNum), Number(secondNum));
+//     alert(`${firstNum} / ${secondNum} = ${Number(firstNum) / Number(secondNum)}`);
+// } else{
+//     alert("please make sure you are selecting the correct numbers and operations");
+// };
